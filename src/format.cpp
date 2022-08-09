d46b99b3aec633faf33830aed17f3584e4913dfb
Joe Abraham <mail.joeabraham@gmail.com>
2022-08-09 11:34:19 +0530

    Correct seconds in ElapsedTime

--- src/format.cpp
+++ src/format.cpp
@@ -8,8 +8,8 @@ using std::to_string;
 string Format::ElapsedTime(long seconds) {
   const int hour = seconds / 3600;
   const int minute = (seconds / 60) % 60;
-  const int seconds = seconds % 60;
+  const int second = seconds % 60;
 
   return string(to_string(hour) + ":" + to_string(minute) + ":" +
-                to_string(seconds));
+                to_string(second));
 }
