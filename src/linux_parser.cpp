bb619cb301a57205474cdb3a441939e4c9dc5351
Joe Abraham <mail.joeabraham@gmail.com>
2022-08-10 10:38:34 +0530

    Updated the code for Jiffies

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -13,6 +13,42 @@ using std::string;
 using std::to_string;
 using std::vector;
 
+vector<string> OpenAndExtractWord(string file, string wanted_key,
+                               bool first_line = false) {
+  string key, line, word;
+  const int word_limit = 25;  // word_limit
+  vector<string> words(word_limit, "0");
+  std::ifstream filestream(file);
+
+  if (filestream.is_open()) {
+    // only read first line if first_line is true
+    if (first_line) {
+      std::getline(filestream, line);
+      std::istringstream stream(line);
+      int i = 0;
+      while (stream >> word && i < word_limit) {
+        words[i] = word;
+        i++;
+      }
+      return words;
+    }
+    // else find the desired key
+    while (std::getline(filestream, line)) {
+      std::istringstream sstream(line);
+      sstream >> key;
+      if (key == wanted_key) break;
+    }
+    std::istringstream stream(line);
+    int j = 0;
+    while (stream >> word && j < word_limit) {
+      words[j] = word;
+      j++;
+    }
+    filestream.close();
+  }
+  return words;
+}
+
 // DONE: An example of how to read data from the filesystem
 string LinuxParser::OperatingSystem() {
   string line;
@@ -108,43 +144,52 @@ long LinuxParser::UpTime() {
   return std::stol(uptime);
 }
 
-// TODO: Read and return the number of jiffies for the system
-long LinuxParser::Jiffies() { return 0; }
+// Read and return the number of jiffies for the system
+long LinuxParser::Jiffies() {
+  return LinuxParser::ActiveJiffies() + LinuxParser::IdleJiffies();
+}
 
-// TODO: Read and return the number of active jiffies for a PID
-// REMOVE: [[maybe_unused]] once you define the function
-long LinuxParser::ActiveJiffies(int pid [[maybe_unused]]) { return 0; }
+// Read and return the number of active jiffies for a PID
+long LinuxParser::ActiveJiffies(int pid) {
+  string filename = kProcDirectory + std::to_string(pid) + kStatFilename;
+  vector<string> words = OpenAndExtractWord(filename, "", true);
+  return std::stol(words[13]) + std::stol(words[14]) + std::stol(words[15]) +
+         std::stol(words[16]);
+}
 
-// TODO: Read and return the number of active jiffies for the system
-long LinuxParser::ActiveJiffies() { return 0; }
+// Read and return the number of active jiffies for the system
+long LinuxParser::ActiveJiffies() {
+  // ActiveJiffies = user + nice + system + irq + softirq + steal
+  vector<string> jiffies =
+      OpenAndExtractWord(kProcDirectory + kStatFilename, "cpu");
+  long user = std::stol(jiffies[1]);
+  long nice = std::stol(jiffies[2]);
+  long system = std::stol(jiffies[3]);
+  long irq = std::stol(jiffies[6]);
+  long softirq = std::stol(jiffies[7]);
+  long steal = std::stol(jiffies[8]);
+  return user + nice + system + irq + softirq + steal;
+}
 
-// TODO: Read and return the number of idle jiffies for the system
-long LinuxParser::IdleJiffies() { return 0; }
+// Read and return the number of idle jiffies for the system
+long LinuxParser::IdleJiffies() {
+  vector<string> idleJiffies =
+      OpenAndExtractWord(kProcDirectory + kStatFilename, "cpu");
+  long idle = std::stol(idleJiffies[4]);
+  long iowait = std::stol(idleJiffies[5]);
+  return idle + iowait;
+}
 
 // Read and return CPU utilization
-vector<string> LinuxParser::CpuUtilization() {
-  std::ifstream fs(kProcDirectory + kStatFilename);
-  string line;
-  float user, nice, system, idle, iowait, irq, softirq, steal;
+float LinuxParser::CpuUtilization() {
+  // The utilisation of all CPU is summed up.
+  long jiffies = LinuxParser::Jiffies();
+  float activeJiffies = 1.0 * LinuxParser::ActiveJiffies();
 
-  while (std::getline(fs, line)) {
-    std::istringstream ls(line);
-    string title;
-    ls >> title;
-    if (title == "cpu") {
-      ls >> user >> nice >> system >> idle >> iowait >> irq >> softirq >> steal;
-    }
+  if (jiffies != 0) {
+    return activeJiffies / jiffies;
   }
-
-  float nonIdleSum{user + nice + system + irq + softirq + steal};
-  float idleSum{idle + iowait};
-
-  float util = nonIdleSum / (nonIdleSum + idleSum);
-
-  vector<string> res;
-  res.push_back(std::to_string(util));
-
-  return res;
+  return 0;
 }
 
 // Read and return the total number of processes
